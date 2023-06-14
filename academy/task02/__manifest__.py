f544f405ad0e113bc64866a44e3c6637b9cdba29
cobr-odoo <cobr@odoo.com>
2023-06-14 10:18:40 -0400

    [ADD] Updated module

--- academy/task02/__manifest__.py
+++ academy/task02/__manifest__.py
@@ -1,11 +1,11 @@
 {
         'name': 'Odoo Academy',
         'summary': """Module to handle Courses and Sessions""",
-        'description: """Module to handle:
+        'description': """Module to handle:
             - Courses
             - Sessions
             - Attendees
-            """',
+            """,
         'license': 'OPL-1',
         'author': 'cobr',
         'website': 'www.odoo.com',
