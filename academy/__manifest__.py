f15e316cb36589e0a85699b5bbf9e0b08108f2d0
cobr-odoo <cobr@odoo.com>
2023-06-14 10:04:05 -0400

    [ADD] New Module

---
+++ academy/__manifest__.py
@@ -0,0 +1,17 @@
+{
+        'name': 'Odoo Academy',
+        'summary': """Module to handle Courses and Sessions""",
+        'description: """Module to handle:
+            - Courses
+            - Sessions
+            - Attendees
+            """',
+        'license': 'OPL-1',
+        'author': 'cobr',
+        'website': 'www.odoo.com',
+        'category': 'Custom Modules/Tech Training',
+        'depends': ['base']
+        'data': [],
+        'demo': [],
+        'application': True,
+}
